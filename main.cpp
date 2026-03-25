9d6994d17ec76e021b134f24bee2d62ac046f81b
yooseungyeon31 <ysy8317433@gmail.com>
2026-03-25 15:14:02 +0900

    예외처리 templete class

--- main.cpp
+++ main.cpp
@@ -4,78 +4,32 @@
 
 using namespace std;
 
-//void Test(int A)
-//{
-//
-//}
 
-//Default Argumnent
-//void Test(int A = 2, int B = 1, int C = 0)
-//{
-//	cout << A << endl;
-//	cout << B << endl;
-//	cout << C << endl;
-//}
+
 
 int main()
 {
-	//Test(1, 2, 3);
+	DynamicArray<int> IntArray;
 
-	//return 0;
+	IntArray.PushBack(1);
+	IntArray.PushBack(2);
+	IntArray.PushBack(3);
 
-	DynamicArray D(4);
-
-	for (int i = 0; i < 8; ++i)
+	for (int i = 0; i < IntArray.GetSize(); ++i)
 	{
-		cout << D.GetSize() << endl;
-		cout << D.GetCapacity() << endl;
-		cout << "----------------------------" << endl;
-
-		D.PushBack(i + 1);
+		cout << IntArray[i] << endl;
 	}
 
-	//[1][2][3][4][5][6][7][8]
-	D.RemoveAt(3);
+	DynamicArray<float> FloatArray;
 
-	cout << D.GetSize() << endl;
-	cout << D.GetCapacity() << endl;
-	cout << "----------------------------" << endl;
-	for (int i = 0; i < D.GetSize(); ++i)
+	FloatArray.PushBack(1.1f);
+	FloatArray.PushBack(2.2f);
+	FloatArray.PushBack(3.3f);
+
+	for (int i = 0; i < FloatArray.GetSize(); ++i)
 	{
-		cout << D[i] << ", ";
+		cout << FloatArray[i] << endl;
 	}
-	//[1][2][3][5][6][7][8]
-
-	cout << endl;
-
-	D.RemoveAt(5);
-
-	cout << D.GetSize() << endl;
-	cout << D.GetCapacity() << endl;
-	cout << "----------------------------" << endl;
-
-
-	for (int i = 0; i < D.GetSize(); ++i)
-	{
-		cout << D[i] << ", ";
-	}
-	//[1][2][3][5][6][8]
-	cout << endl;
-
-	D.PushBack(9);
-
-	for (int i = 0; i < D.GetSize(); ++i)
-	{
-		cout << D[i] << ", ";
-	}
-	//[1][2][3][5][6][8][9]
-	cout << endl;
-
-	cout << D.GetSize() << endl;
-	cout << D.GetCapacity() << endl;
-	cout << "----------------------------" << endl;
-
-
 
 
 
